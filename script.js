182b666e19c06db36b193a38a46b0b4f762180a1
nothing here <roninfortnite71@gmail.com>
2024-11-21 18:55:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+/* console.log(1); */
+"use strict";
+
+a = 15;
+console.log(a);
+
+let number = 5; /* первый способ объявления переменной */
+const leftBorderWidth = 1; /* второй способ объявления переменной */
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);   
+
+{
+    let result = 50;
+}
+
+console.log(result);
+
+/* alert(5);
+[].push('a'); */
+
+
